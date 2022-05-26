f1b6ce15bdd41d38a055c35f85d80b494ccc6d43
perunon <patryk.pilarczyk.00@gmail.com>
2022-05-26 14:44:51 +0200

    Style the entire app

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -7,13 +7,30 @@ const StatusMessage = ({ winner, current }) => {
   //     : `The next player is ${current.isXNext ? 'X' : 'O'}`;
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `The next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && `X and O tied`}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          The next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange"> O</span> tied
+        </>
+      )}
+    </div>
   );
 };
 
